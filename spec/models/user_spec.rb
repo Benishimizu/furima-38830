07550a430609b6bdec759c1ec622d6dfcbab9075
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-04-24 22:27:19 +0900

    user_spec.rbを編集
    ◎beforeブロックを作成し、インスタンスの生成を行いましょう。
    
    理由：テストに必要なインスタンスはbeforeブロックで実装されることが好ましいためです。
    
    @yuri240 yuri240 yesterday
    正常系テストが未実行のようです。追加でテストを行いましょう！
    
    またその際は、
    contextを用いて「登録できる場合」と「登録できない場合」
    にまとめていただくとより効率的で見やすいコードとなります。
    
    よろしければ、下記カリキュラムを参考にしてください。
    https://master.tech-camp.in/v2/curriculums/4795
    
    context
    特定の条件を分けたい場合に用います。使用方法はdescribeと同じですが、describeはどのような「機能」について確認するのか概要を書き、contextはどのような「状況」を確認したいのかを書きます。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      end
+      it "nicknameが6文字以下で登録できる" do
+      end
+      it "passwordが6文字以上であれば登録できる" do
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "nicknameが7文字以上であれば登録できない" do
+
+
+        
+      end
+      it "emailが空では登録できない" do
+      end
+      it "重複したemailが存在する場合登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "passwordが5文字以下であれば登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      end
+    end
+  end
+end
+
+
+
+
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       user = FactoryBot.build(:user)  # Userのインスタンス生成
@@ -59,3 +105,21 @@ RSpec.describe User, type: :model do
   end
 end
 
+
+
+
+
+・「パスワード」が英数字混合であること。
+
+
+＝＝＝＝＝＝
+・「名字・名前」が全角日本語であること
+it 'お名前(全角)が全角日本語であること' do
+  user = FactoryBot.build(:user)  # Userのインスタンス生成
+  user.iv_f_name = ''  # firstnameの値を空にする
+  user.valid?
+  expect(user.errors.full_messages).to include "Iv f name can't be blank"
+end
+
+
+・「名字(カナ)・名前(カナ)」が全角カタカナであること。
