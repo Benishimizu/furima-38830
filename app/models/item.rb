700adc85d708fc5fec81be34b7ecec449eb35ac0
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-04-29 21:47:46 +0900

    手順3 ActiveHashを導入する
    バリデーションを設定しよう
    データベースに空の投稿が保存されないようにバリデーションを設定します。
    
    今回のバリデーションには、「numericality」というバリデーション のヘルパーを使用します。
    
    numericalityニューメリカリティ
    numericalityとは、数値かどうかを検証するバリデーションの一種です。
    数値であればデータベースに保存を許可して、それ以外では保存が許可されないようにできます。
    今回のアプリケーションでは、---を保存されないようにしたいので、id: 1以外であれば保存できるように設定します。
    
    バリデーションを設定しましょう
    以下、二つのバリデーションを設定しましょう。
    9行目はgenre_idに関するバリデーションです。
    このバリデーションは、genre_idのid:1以外のときに保存できるという意味です。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,20 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_description
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_term
+
+    #空の投稿を保存できないようにする
+    validates :title, :text, presence: true
+
+    #ジャンルの選択が「---」の時は保存できないようにする
+    validates :genre_id, numericality: { other_than: 1 } 
+
+
+    
+
 end
