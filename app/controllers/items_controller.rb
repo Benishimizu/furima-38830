73bf4ac3585d7b0640ad0ccca3bad1bd23a337da
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-04-30 21:07:05 +0900

    手順6 出品した商品の情報をテーブルに保存できるようにする
    また、入力した商品情報がバリデーションを通過しなかった場合、再度「商品出品ページ」が表示されるように実装しましょう。
    バリデーション通過の可否を条件に処理を分岐させ、エラーメッセージが表示されるように適切なメソッドを用いて記述してください。
    
    =====
    
        if @item.save
          redirect_to root_path
        else
          redirect_to new_item
          # render :new
        end
    
        redirect_to new_itemにした際、＠itemにエラーメッセージを入れてくれる→redirectはルーティング、コントローラーのMVCを通るので   @item = Items.newを通ってしまうのであtらしく上書きしてしまう→エラーメッセージが表示されない
    
    render；はこの場でVIEWファイルを表示する
    
    ＊renderとredirect toの違い

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,23 @@ class ItemsController < ApplicationController
 
 
   def new
+    @item = Item.new
 
   end
 
   def create
+
+    @item = Item.new(item_params)
+
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+
+
 
   end
 
