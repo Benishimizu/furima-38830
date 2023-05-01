8da900b9b2017e9654a4412ce98b29c6abac3fd4
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-01 17:48:15 +0900

    手順7 モデルの単体テストコードを書く
    https://teratail.com/questions/308588?sort=1
    
    spec/factories/items.rbへの書き方
    
    ActiveHASHを利用したときのspec/factories/items.rbへの書き方
       item_description_id  {1}と書いてみた

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :item do
+
+
+        image         {Faker::Lorem.sentence}
+        item_name          {"名前"}
+        description    {"説明"}
+
+        price           {2000}
+        item_category_id  {1}
+        item_description_id         {1}
+        prefecture_id      {1}
+        shipping_fee_id         {1}
+        shipping_term_id        {1}
+        association :user
     
+
+
   end
 end
+
+
