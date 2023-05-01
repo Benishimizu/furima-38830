a693f2320b10e8ff6c1bdb374316bddaca4a5274
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-02 07:36:28 +0900

    ログアウト状態の場合は、商品出品ページへ遷移しようとすると、ログインページへ遷移する動画
    https://qiita.com/taka_no_okapi/items/5d33cf83e21a7ade0943
    
    https://teratail.com/questions/302103
    
    https://ja.stackoverflow.com/questions/63438/%E4%BB%96%E4%BA%BA%E3%81%AE%E7%B7%A8%E9%9B%86%E7%94%BB%E9%9D%A2%E3%81%AB%E9%81%B7%E7%A7%BB%E3%81%A7%E3%81%8D%E3%81%AA%E3%81%84%E3%82%88%E3%81%86%E3%81%AB%E3%81%97%E3%81%9F%E3%81%84

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_signed_in, expect: [:index]
 
 
     def index
-      @items = Item.all
+      @item = Item.all
+      if current_user == @item.user
+        redirect_to root_path
+        end
     end
 
   def new
@@ -28,5 +32,43 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name,:price, :description, :item_category_id, :item_description_id, :prefecture_id, :shipping_fee_id, :image, :shipping_term_id).merge(user_id: current_user.id)
   end
 
-end
+# end
 
+# =====================
+# class PurchasesController < ApplicationController
+#   before_action :move_to_signed_in, expect: [:index]
+
+#   def index
+#     @donation = Donation.new
+#     @item = Item.find(params[:item_id])
+#   end
+
+#   def new
+#     @donation = Donation.new
+#     @item = Item.find(params[:item_id])
+#   end  
+
+#   def create
+#     @item = Item.find(params[:item_id])
+#     @donation = Donation.new(purchase_params)
+#     if @donation.save
+#       redirect_to root_path
+#     else
+#       render :new
+#     end
+
+
+#   end
+
+#   private
+
+#   def purchase_params
+#     params.require(:donation).permit(:post_code, :city, :block_number, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id])
+#   end  
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
+  end
+end
