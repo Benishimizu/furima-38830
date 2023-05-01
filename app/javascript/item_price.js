a08238fe5a5924b123e5011edf06e983d563e6c3
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-02 07:56:49 +0900

    createアクション失敗時のエラーメッセージ表示について
    https://qiita.com/kidokoro-syusei/items/2a2ad0cccc9b470e2bd3
    
    https://note.com/tumu1632/n/n6bc357c88d68

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,17 +4,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    // console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue*0.1);
-    // const addTax = addTaxDom.value;
-    // const addTaxValue = addTax.value;
   
   
     const profitDom = document.getElementById("profit");
     profitDom.innerHTML = Math.floor(inputValue - (Math.floor(inputValue*0.1)));
-    // console.log(profitDom);
   
 
   })
