34f933ef17732534d65d38a87b6eabc890d44788
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-01 22:55:00 +0900

    手順3 入力するたびにイベントを発火できるようにする
    金額を入力するたびに、イベント発火するようにaddEventListenerを使用します。
    addEventListenerは、イベント発火の際に実行する関数を定義するためのメソッドです。
    今回は、入力があるたびにイベント発火を起こしたいため、以下のようにinputというイベントを使用します。
    
    【例】item_price.js
    1
    2
    3
    4
    const priceInput = document.getElementById("金額を入力する場所のid");
    priceInput.addEventListener("input", () => {
    
    })
    次に、console.logを記述して、入力するたびにイベント発火されるか確認をします。
    
    【例】item_price.js
    1
    2
    3
    4
    const priceInput = document.getElementById("金額を入力する場所のid");
    priceInput.addEventListener("input", () => {
       console.log("イベント発火");
    })
    商品出品ページで金額を入力し、コンソール上に"イベント発火"と表示されるか確認をします。
    
    https://tech-master.s3.amazonaws.com/uploads/curriculums//7138cf5255d29dfab8404394768bcfd2.gif
    
    もし"イベント発火"という文字列がコンソール上に表示されない場合は、以下の項目を確認しましょう。
    
    そもそもJSファイルが読み込めているか
    turbolinksの記述が残っていないか
    JSファイルのコードの記述に間違いがないか

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,13 +2,20 @@ window.addEventListener('load', () => {
   console.log("OK");
 
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
 
-  const priceContent = document.getElementById("add-tax-price");
-  console.log(priceContent);
+  })
 
-  const priceContents = document.getElementById("profit");
-  console.log(priceContent);
+  // const priceContent = document.getElementById("add-tax-price");
+  // console.log(priceContent);
+
+  // const priceContents = document.getElementById("profit");
+  // console.log(priceContent);
+
+
+
+
 
 });
 
