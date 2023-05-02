ca8350205c6bbf48053f0429e6afa50147c33235
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-02 21:31:51 +0900

    with: /\A[0-9]+\z/
    with: /\A[0-9]+\z/
    上記の記述を削除し、整数値のみ(小数は登録できない)登録できるようなバリデーションを設定しましょう。
    
    理由：
    データ型がstringなどの文字列に対して正規表現のバリデーションを設定することが可能です。
    しかし、priceカラムのデータ型はintegerであるため、正規表現でのバリデーションは設定することができなくなってしまうためです。
    
    「rails バリデーション 整数値」などで外部検索をしていただくのもおすすめです。

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,19 @@ class Item < ApplicationRecord
     validates :image, presence: true
 
     validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+    with: /\A[0-9]+\z/
+    integerがたでは反映されない→やりたいことが叶わない
+
+
+    validates :price, numericality: {only_integer: true, message: 'Half-width number'}
+
     validates :price,
               numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+
+
+    validates :price,
+              numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+
+
 
 end
