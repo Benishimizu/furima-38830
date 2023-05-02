2baf7052cbdd173dc2b54b323cca953f5a2ef6de
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-02 21:35:11 +0900

    書き方
    https://qiita.com/syohhe/items/ecef500da2cd7956489d
    
    ３４・３５行目は２８ー３１行目を合体させてエラー文章を除いたもの
    どちらのコードでも問題ないが、今回はエラー分がほしいため最初を優先する
    
    また、２４行目にPRECENCE：TRU Eとしているので、２８ー３１行目をには設定しなくてOK

--- app/models/item.rb
+++ app/models/item.rb
@@ -31,9 +31,6 @@ class Item < ApplicationRecord
               numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
 
-    validates :price,
-              numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
-
 
 
 end
