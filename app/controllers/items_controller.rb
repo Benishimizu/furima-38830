ff2528fd5d513b6b8a00f3090ce5d622e7174ec5
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-07 16:29:54 +0900

    Update items_controller.rb
    https://teratail.com/questions/287231

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
+
   end
 
   private
