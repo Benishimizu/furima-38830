73392ffc0dce09577dd88ba148ec867eaf154d8b
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-10 08:12:04 +0900

    バリデーションの処理を書きましょう
    donationモデルとaddressモデルにあるバリデーションの記述を、Formオブジェクトへ移動させます。
    
    ここまでの実装は、Formオブジェクトの属性をform_withに対応させて、テーブルへ保存できるように進めてきました。
    
    この後の実装ではテーブルへ保存する際に、Formオブジェクトのインスタンスに対してバリデーションを実行します。
    そのために、Formオブジェクトにバリデーションを定義しておく必要があります。
    
    donation_address.rbに以下のように記述しましょう。
    
    app/models/donation_address.rb
     1
     2
     3
     4
     5
     6
     7
     8
     9
    10
    11
    class DonationAddress
      include ActiveModel::Model
      attr_accessor :price, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name
    
      with_options presence: true do
        validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
        validates :user_id
        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
      end
      validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
    end
    user_idに対して、presence: trueのバリデーションを新たに追加しています。
    
    donationsテーブルに保存されるuser_idには、本来belongs_to :userのアソシエーションにより、バリデーションが設定されています。
    すなわち、Donationモデルがインスタンスを保存する際に紐付くユーザー情報がない場合、モデル内のアソシエーションの記述により保存できません。
    
    しかし、donation_addressクラスにはアソシエーションを定義することはできないため、belongs_toによるバリデーションを行うことができません。
    そこで、donation_addressクラスでuser_idに対してバリデーションを新たに設定しました。
    
    donation.rbとaddress.rbのバリデーションは不要になるため、削除しておきましょう。
    
    app/models/donation.rb
    1
    2
    3
    4
    5
    6
    class Donation < ApplicationRecord
      belongs_to :user
      has_one :address
    
      # validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is invalid"}
    end
    app/models/address.rb
    1
    2
    3
    4
    5
    6
    class Address < ApplicationRecord
      belongs_to :donation
    
      # validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
      # validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
    end

--- app/models/order_sender.rb
+++ app/models/order_sender.rb
@@ -1,6 +1,16 @@
 class OrderSender
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :detailed_address, :building, :tel, :item, :user
-end
+
+  with_options presence: true do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
+    validates :user
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :item
+  validates :city
+  validates :detailed_address
+
 
 end
