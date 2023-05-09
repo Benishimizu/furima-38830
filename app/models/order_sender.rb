8b66b96cbd535f9a5c68865008f6a2f09224bfe6
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-10 08:26:07 +0900

    手順1-5. データをテーブルに保存する処理を書く
    データをテーブルに保存する処理を書きましょう
    Formオブジェクトに、フォームから送られてきた情報をテーブルに保存する処理を記述します。
    
    app/models/donation_address.rb
     1
     2
     3
     4
     5
     6
     7
     8
     9
    10
    11
    12
    13
    14
    15
    16
    17
    18
    19
    class DonationAddress
      include ActiveModel::Model
      attr_accessor :price, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name
    
      with_options presence: true do
        validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is invalid"}
        validates :user_id
        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
      end
      validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
    
      def save
        # 寄付情報を保存し、変数donationに代入する
        donation = Donation.create(price: price, user_id: user_id)
        # 住所を保存する
        # donation_idには、変数donationのidと指定する
        Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
      end
    end

--- app/models/order_sender.rb
+++ app/models/order_sender.rb
@@ -13,4 +13,18 @@ class OrderSender
   validates :detailed_address
 
 
+  def save
+    # # 各テーブルにデータを保存する処理を書く
+    # 寄付情報を保存し、変数donationに代入する
+    # donation = Donation.create(price: price, user_id: user_id)
+     # 注文情報を保存し、変数donationに代入する
+    order = Order.create(item: item, user: user)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    # Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
+    Sender.create(postal_code: postal_code, prefecture_id:prefecture_id, city:city, detailed_address:detailed_address, building:building, tel:tel,)
+  
+  end
+
+
 end
