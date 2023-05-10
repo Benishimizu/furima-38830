a519593fa5630a90ae91dab9d258841cbbcdb0ef
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-10 21:39:54 +0900

    RECHECK
    with_options presence: true do
    end
    ->
    with_optionsウィズ オプションズ
    with_optionsは複数のバリデーションをグループ化し、共通の制限をかけるためのメソッドです。
    
    さらに、formatヘルパーを使用することで、正規表現を用いた条件を設定することができます。

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,8 +25,8 @@ class Item < ApplicationRecord
 
     validates :price, numericality: {only_integer: true, message: 'Half-width number'}
 
-    # validates :price,
-    #           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+    validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
 
 
