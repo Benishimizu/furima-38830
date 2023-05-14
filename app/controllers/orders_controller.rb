775e0cad5384e796c308692a3cc992efedde4172
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-14 16:07:04 +0900

    ①ブラウザのカード情報を取得する
    まずは、ブラウザに入力されたカード情報を取得する必要があります。このとき意識するべきポイントは以下の3点です。
    
    購入ボタンを押した際に、イベントが発火していること
    カード情報入力フォームのDOM（HTMLの要素）がJavaScript側で取得できていること
    取得したDOMからフォームに入力した値がJavaScript側で取得できていること
    購入ボタンを押した際に、イベントが発火していること
    まずは、購入ボタンを押した際にイベント発火の処理が行われるように実装する必要があります。なぜならば、この時点でイベント発火が起きていなければ、この後どれだけ実装しても、イベント発火の中に記述した処理が実行されることはないからです。
    
    イベント発火を実装できたら、console.log()を用いてイベントが問題なく発火することを確認しましょう。
    
    カード情報入力フォームのDOM（HTMLの要素）がJavaScript側で取得できていること
    イベントが問題なく発火することが確認できたら、次はカード情報入力フォームのDOMが取得できていることをそれぞれ確認します。
    
    取得したDOMからフォームに入力した値がJavaScript側で取得できていること
    最後に、カード情報入力フォームのDOMからフォームに入力した値をそれぞれ取得します。
    
    Template is missingのエラー解消のため一旦コメントアウト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,28 +16,28 @@ def index
 # Normally new, create and form's model option should be same
 
 
-  def create
-      # binding.pryは処理に不要なので削除する
-      # 値をDBへ保存する実装
+#   def create
+#       # binding.pryは処理に不要なので削除する
+#       # 値をDBへ保存する実装
 
-    binding.pry
-    @order_sender = OrderSender.new(sender_params)
-    if @order_sender.valid?
-      @order_sender.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+#     binding.pry
+#     @order_sender = OrderSender.new(sender_params)
+#     if @order_sender.valid?
+#       @order_sender.save
+#       redirect_to root_path
+#     else
+#       render :new
+#     end
+#   end
 
-  private
+#   private
 
-  def sender_params
-    params.require(:order_sender).permit(:postal_code, :prefecture_id, :city, :detailed_address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
-# requireに書きたいのはformオブジェクトの情報（２つの情報をまとめたいから）
-    # permitはフォームから送られてくる内容を書く→今回は住所情報
-# mergeで書く内容は外部情報を送りたい時に書く→今回は購入情報がorderの情報になるからordersテーブルに保存したい外部キーつまり、users とitemになる
-  end
+#   def sender_params
+#     params.require(:order_sender).permit(:postal_code, :prefecture_id, :city, :detailed_address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+# # requireに書きたいのはformオブジェクトの情報（２つの情報をまとめたいから）
+#     # permitはフォームから送られてくる内容を書く→今回は住所情報
+# # mergeで書く内容は外部情報を送りたい時に書く→今回は購入情報がorderの情報になるからordersテーブルに保存したい外部キーつまり、users とitemになる
+#   end
 
 
 end
