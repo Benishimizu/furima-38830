771d77625ca91b013cdb9505dc51ff40b0a96bc4
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-15 00:00:08 +0900

    NameError (undefined local variable or method `order_id' for #<OrderSender:0x00007f82ed38fc08> Did you mean?  order):
    NameError (undefined local variable or method `order_id' for #<OrderSender:0x00007f82ed38fc08>
    Did you mean?  order):を解消するには外部キーの解消が必要

--- app/models/order_sender.rb
+++ app/models/order_sender.rb
@@ -11,10 +11,28 @@ class OrderSender
     validates :city
   end
     # validates :detailed_address
+    # テストコードstep7
+    # OrderSEnderののバリデーションを見ながらテストコードを書くよ
+    # テストに足りない情報があれば追加すればOK
 
   def save
-    order = Order.create(item_id: item_id, user: user)
-    Sender.create(postal_code: postal_code, prefecture_id:prefecture_id, city:city, detailed_address:detailed_address, building:building, tel:tel)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    # binding.pry
+    Sender.create(postal_code: postal_code, prefecture_id:prefecture_id, city:city, detailed_address:detailed_address, building:building, tel:tel, order:order)
+    #       t.references      :order, null: false, foreign_key: true
+# が足りないだからsenderに保存ができない
+# --> どうしたらいいかというと 上記のorderを利用して外部キーに値を入れたい
+
+# そのために下記の様にすることが必要である　SMALLSTEP1
+# # 寄付情報を保存し、変数donationに代入する
+# donation = Donation.create(price: price, user_id: user_id)
+# # 住所を保存する
+# # donation_idには、変数donationのidと指定する
+# Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
+# end
+# end
+
+
   
   end
 
