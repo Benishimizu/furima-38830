c3ed3dca912991a671a18a6385adaf8c002622cf
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-15 23:53:04 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,10 +1,40 @@
+// const pay = () => {
+//   const payjp = Payjp('pk_test_c24e16fb276bf20b852e85e3')// PAY.JPテスト公開鍵
+//   const submit = document.getElementById("button");
+//   submit.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     console.log("フォーム送信時にイベント発火")
+//   });
 const pay = () => {
   const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const submit = document.getElementById("button");
+
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 
 window.addEventListener("load", pay);
+// Step12コメントアウトを削除
