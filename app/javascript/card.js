3af7a19913d8dfdd67b3bbbfd3aeef4e191d1c46
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-15 21:41:06 +0900

    Step10

---
+++ app/javascript/card.js
@@ -0,0 +1,21 @@
+// Step10作成してーー＞Done！
+
+
+
+// そして、application.jsにcard.jsを読み込むための記述をしましょう。以下のように編集してください。
+
+// app/javascript/packs/application.js
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// // 省略
+// require("@rails/ujs").start()
+// // require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("../card")
+// // 省略
