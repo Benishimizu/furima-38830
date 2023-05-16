a7e258d4648cee5a520b82e6b4d0f7b8a36791d0
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-16 20:59:27 +0900

    サーバーサイドで環境変数を読み込めるようにしよう
    数の設定ができたので、サーバーサイド（今回はコントローラー）から環境変数を呼び出せるようにコードを編集しましょう。
    
    秘密鍵代入した環境変数を呼び込みましょう
    コントローラーを以下のように編集しましょう。
    
    app/controllers/orders_controller.rb
    1
    2
    3
    4
    5
    6
    7
    8
    9
    #省略
    def pay_item
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: order_params[:price],
         card: order_params[:token],
         currency:'jpy'
       )
    end
    これまで直接秘密鍵を指定していたところを、環境変数に置き換えました。
    
    クライアントサイドで環境変数を読み込めるようにしよう
    クライアントサイド、つまりJavaScriptのファイルには公開鍵が設定されています。厳密に言えば、公開鍵についてはそのままコード上に記述していても問題ありません。しかしながら、「鍵情報は外部に公開しないようにする」を癖付けるためにも、今回は公開鍵も環境変数として設定し、JavaScriptのファイルから読み込むようにします。
    
    JavaScriptで環境変数を呼び込みましょう
    今までの記述では、Railsで設定した環境変数をJavaScriptで呼び出せません。その際は、下記のようにwebpackerを用いることで呼び出せます。
    
    まず、以下のコマンドを実行してwebpacker.rbを作成してください。
    
    ターミナル
    1
    % touch config/initializers/webpacker.rb
    次に、以下のように編集してください。
    
    config/initializers/webpacker.rb
    1
    Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
    app/javascript/card.js
    1
    2
    3
    const pay = () => {
    const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
      // 省略
    これで、環境変数を呼び出すことができました。サーバーを再起動して問題なく動作するか確認しましょう。
    
    開発環境での環境変数の設定はこれでおしまいです。
    本番環境のアプリで決済機能を利用する場合も、開発環境で行ったのと同様に公開鍵・秘密鍵を環境変数に設定する必要があります。
    ここからは本番環境に環境変数を設定する方法を紹介します。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ def index
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_08e0f8992696d402ff15a9f7"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: order_params[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
