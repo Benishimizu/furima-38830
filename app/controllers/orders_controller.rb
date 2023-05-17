577c4a8047571dc42afca565d0f1df9f82e4d46d
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-17 20:01:59 +0900

    コメントアウト削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,9 +30,9 @@ def index
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: sender_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: sender_params[:token],   
+      currency: 'jpy'             
     )
   end
 
