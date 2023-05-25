ab3dfde6dbf98c24a8ee434b5d8425dd611b567b
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-05-25 22:10:19 +0900

    test
    id_ : NOに要注意

--- app/models/order_sender.rb
+++ app/models/order_sender.rb
@@ -5,11 +5,11 @@ class OrderSender
   with_options presence: true do
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含めた半角文字列のみ保存です"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "は必須です"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "を入力してください"}
     validates :item_id
     validates :city
     validates :detailed_address
-    validates :tel, format:{with:/\A\d{10,11}\z/, message: "は登録できません."}
+    validates :tel, format:{with:/\A\d{10,11}\z/, message: "は登録できません"}
     validates :token
 
   end
--- spec/models/order_sender_spec.rb
+++ spec/models/order_sender_spec.rb
@@ -23,68 +23,68 @@ RSpec.describe OrderSender, type: :model do
       it '郵便番号が必須であること。' do
         @order_sender.postal_code= ''
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("郵便番号は必須です")
+        expect(@order_sender.errors.full_messages).to include("郵便番号を入力してください")
 
       end
       it ' 郵便番号は、「3桁ハイフン4桁」の半角文字列のみ保存可能なこと（良い例:123-4567 良くない例:1234567）。' do
         @order_sender.postal_code='1234567'
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@order_sender.errors.full_messages).to include("郵便番号はハイフンを含めた半角文字列のみ保存です")
       end
       it '都道府県が必須であること。' do
-        @order_sender.prefecture_id= '0'
+        @order_sender.prefecture_id= '1'
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@order_sender.errors.full_messages).to include("都道府県を入力してください")
       end
       it '市区町村が必須であること。' do
         @order_sender.city=''
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("City can't be blank")
+        expect(@order_sender.errors.full_messages).to include("市町村を入力してください")
       end
       it '番地が必須であること。' do
         @order_sender.detailed_address=''
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Detailed address can't be blank")
+        expect(@order_sender.errors.full_messages).to include("住所詳細を入力してください")
       end
       it '電話番号が必須であること。' do
         @order_sender.tel=''
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Tel can't be blank")        
+        expect(@order_sender.errors.full_messages).to include("電話番号を入力してください")        
 
       end
       it '電話番号は、半角数字以外が含まれている場合、登録できないこと' do
         @order_sender.tel='123456789２s'
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include('Tel is invalid.')
+        expect(@order_sender.errors.full_messages).to include('電話番号は登録できません')
       end
       it '電話番号は、9桁以下では登録できないこと' do
         @order_sender.tel='12345678'
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include('Tel is invalid.')
+        expect(@order_sender.errors.full_messages).to include('電話番号は登録できません')
       end
       it '電話番号は、12桁以上では登録できないこと' do
         @order_sender.tel='1234567891234'
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include('Tel is invalid.')
+        expect(@order_sender.errors.full_messages).to include('電話番号は登録できません')
       end
 
 
       it "tokenが空では登録できないこと" do
         @order_sender.token =''
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Token can't be blank")
+        expect(@order_sender.errors.full_messages).to include("トークンを入力してください")
       end
 
       it "user_idが空だと登録できない" do
         @order_sender.user_id = nil
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("User can't be blank")
+        expect(@order_sender.errors.full_messages).to include("ユーザー名を入力してください")
 
       end
       it "item_idが空だと登録できない" do
         @order_sender.item_id = nil
         @order_sender.valid?
-        expect(@order_sender.errors.full_messages).to include("Item can't be blank")
+        expect(@order_sender.errors.full_messages).to include("商品を入力してください")
       end
 
       
