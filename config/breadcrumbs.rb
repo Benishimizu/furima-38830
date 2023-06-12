567eccbe35b64f2862bc09613346df9996f8f9aa
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-06-12 23:23:00 +0900

    memo
    https://pikawaka.com/rails/gretel
    
    https://ichigick.com/rails-routes/
    
    https://pikawaka.com/rails/gretel

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,28 +2,13 @@ crumb :root do
   link "Top", root_path
 end
 
-crumb :item do
+crumb :items do
   link "商品詳細", item_path
   parent :root
 end
 
 crumb :item_orders do 
-  link "購入ページ", item_orders_path(@item.id)
-  # item_orders_path(@item.id)
-  # item_orders_path.find(params[:id])
-  # item_orders_path(@item.id)
-  # :@itemからidをとる
-
-  # .find(params[:id])
-  # :パラムスからidをとる
-  # MVCの流れを考えた時にビューから飛んでコントローラーで値を取ってくるから一番自然な書き方
-  # FIND:モデルがあってそのPARAMSから探す
-
-  # .find_by(id: params[:id])
-  # :.find(params[:id])と似ているがfind_byになるとID以外も検索できる
-  # つまり類似している
-
-
-
-  parent :item
+  link "購入ページ",  OrderSender(params[:id])
+  
+  parent :items
 end
